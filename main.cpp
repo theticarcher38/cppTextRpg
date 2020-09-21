1f377fc7e4eabd4bfbb9a2568091b3acfa995e9c
theticarcher38 <70581964+theticarcher38@users.noreply.github.com>
2020-09-21 06:28:57 -0500

    Health does not display properly
    Health displays wrong. this is the only known bug. It must be fixed

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@ using namespace std;
 
 int main()
 {
+    //changes the color of the console
+    system("COLOR 5");
+
     string command;
 
     //Melee Weapon Constructor
@@ -38,12 +41,7 @@ int main()
     struct player
     {
         string name;
-        int headHealth;
-        int torsoHealth;
-        int leftArmHealth;
-        int rightArmHealth;
-        int leftLegHealth;
-        int rightLegHealth;
+        int health;
         int level;
         int attributePoints;
         int cash;
@@ -57,12 +55,7 @@ int main()
 
     } player;
 
-    player.headHealth = 10 + player.constitution;
-    player.torsoHealth = 30 + player.constitution;
-    player.leftArmHealth = 20 + player.constitution;
-    player.rightArmHealth = 20 + player.constitution;
-    player.leftLegHealth = 20 + player.constitution;
-    player.rightLegHealth = 20 + player.constitution;
+    player.health = 30;
     player.level = 1;
     player.attributePoints = 30;
     player.strength = 0;
@@ -76,6 +69,9 @@ int main()
 
     string playerName;
     player.name = playerName;
+
+    int playerHealth;
+    player.health = playerHealth;
 
     int playerStrength;
     player.strength = playerStrength;
@@ -105,44 +101,90 @@ int main()
     //Strength Choice
     cout << "How many points do you wish to place into Strength? > ";
     cin >> playerStrength;
-    player.attributePoints = player.attributePoints - playerStrength;
     if (player.attributePoints < playerStrength) {
         cout << "You do not have that many points to spend. \n";
         cout << "How many points do you wish to place into Strength? > ";
         cin >> playerStrength;
     }
     cout << "You placed " << playerStrength << " points into strength \n";
-    cout << "You have " << player.attributePoints << " Attribute Points left \n";
+    player.attributePoints = player.attributePoints - playerStrength;
+    cout << "You have " << player.attributePoints << " Attribute Points left \n\n";
 
     //Dexterity Choice
     cout << "How many points do you wish to place into Dexterity? > ";
     cin >> playerDexterity;
-    player.attributePoints = player.attributePoints - playerDexterity;
     if (player.attributePoints < playerDexterity) {
     cout << "You do not have that many points to spend. \n";
     cout << "How many points do you wish to place into Dexterity? > ";
     cin >> playerDexterity;
     }
-    cout << "You placed " << playerDexterity << " points into Dexterity";
-    cout << "You have " << player.attributePoints << " Attribute Points left \n";
+    cout << "You placed " << playerDexterity << " points into Dexterity \n";
+    player.attributePoints = player.attributePoints - playerDexterity;
+    cout << "You have " << player.attributePoints << " Attribute Points left \n\n";
 
     //Constitution Choice
 
+    cout << "How many points do you wish to place into Constitution? > ";
+    cin >> playerConstitution;
+    if (player.attributePoints < playerConstitution) {
+    cout << "You do not have that many points to spend. \n";
+    cout << "How many points do you wish to place into Constitution? > ";
+    cin >> playerConstitution;
+    }
+    cout << "You placed " << playerConstitution << " points into Constitution \n";
+    player.attributePoints = player.attributePoints - playerConstitution;
+    cout << "You have " << player.attributePoints << " Attribute Points left \n\n";
 
     //Intelligence Choice
 
+    cout << "How many points do you wish to place into Intelligence? > ";
+    cin >> playerIntelligence;
+    if (player.attributePoints < playerIntelligence) {
+    cout << "You do not have that many points to spend. \n";
+    cout << "How many points do you wish to place into Intelligence? > ";
+    cin >> playerIntelligence;
+    }
+    cout << "You placed " << playerIntelligence << " points into Intelligence \n";
+    player.attributePoints = player.attributePoints - playerIntelligence;
+    cout << "You have " << player.attributePoints << " Attribute Points left \n\n";
 
     //Wisdom Choice
 
+    cout << "How many points do you wish to place into Wisdom? > ";
+    cin >> playerWisdom;
+    if (player.attributePoints < playerWisdom) {
+    cout << "You do not have that many points to spend. \n";
+    cout << "How many points do you wish to place into Wisdom? > ";
+    cin >> playerWisdom;
+    }
+    cout << "You placed " << playerWisdom << " points into Wisdom \n";
+    player.attributePoints = player.attributePoints - playerWisdom;
+    cout << "You have " << player.attributePoints << " Attribute Points left \n\n";
 
     //Charisma Choice
 
+    cout << "How many points do you wish to place into Charisma? > ";
+    cin >> playerCharisma;
+    if (player.attributePoints < playerCharisma) {
+    cout << "You do not have that many points to spend. \n";
+    cout << "How many points do you wish to place into Charisma? > ";
+    cin >> playerCharisma;
+    }
+    cout << "You placed " << playerCharisma << " points into Charisma \n";
+    player.attributePoints = player.attributePoints - playerCharisma;
+    cout << "You have " << player.attributePoints << " Attribute Points left \n\n";
 
     cout << "Please enter a command > ";
     cin >> command;
 
-    if (command == "show stats") {
-        cout << "Head Health: " << player.headHealth << "\n";
+    if (command == "!stats") {
+        cout << "Health: " << playerHealth << "\n";
+        cout << "Strength: " << playerStrength << "\n";
+        cout << "Dexterity: " << playerDexterity << "\n";
+        cout << "Constitution: " << playerConstitution << "\n";
+        cout << "Intelligence: " << playerIntelligence << "\n";
+        cout << "Wisdom: " << playerWisdom << "\n";
+        cout << "Charisma: " << playerCharisma << "\n";
     }
 
 }
