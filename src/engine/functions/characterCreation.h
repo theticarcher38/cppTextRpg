b06d66d7882d5ca923cac3b7cee36f93107b9587
theticarcher38 <hunterleegoodman@gmail.com>
2021-04-15 15:54:51 +0000

    Got Attribute Points working
    Signed-off-by: theticarcher38 <hunterleegoodman@gmail.com>

--- src/engine/functions/characterCreation.h
+++ src/engine/functions/characterCreation.h
@@ -1,39 +1,67 @@
 using namespace std;
 
-void characterCreation() {
+int attribInput;
+int plrCInput;
 
-    int plrCInput;
+void attributePoints() {
+    cout << "\nHow many points would you like to allocate? ";
+    cin >> attribInput;
+
+    plrAP = plrAP - attribInput;
+
+    cout << "\nYou have " << plrAP << " points left to allocate \n";
+}
+
+void characterCreation() {
     cout << "What would you like to name your character? ";
     cin >> plrName;
 
     plrAP = 30;
-
-    cout << "You have 30 points to spend in 6 different stats:" << "\n";
-    cout << "[1] Strength    " << "\n"
-         << "[2] Dexterity   " << "\n"
-         << "[3] Constitution" << "\n"
-         << "[4] Intelligence" << "\n"
-         << "[5] Wisdom      " << "\n"
-         << "[6] Charisma    " << "\n";
-    cin >> plrCInput;
-    switch (plrCInput) {
-        case 1:
-        // strength
-            break;
-        case 2:
-        // dexterity
-            break;
-        case 3:
-        // constitution
-            break;
-        case 4:
-        // intelligence
-            break;
-        case 5:
-        // wisdom
-            break;
-        case 6:
-        // charisma
-            break;
+    while(plrAP > 0) {
+        cout << "[1] Strength    " << "\n"
+             << "[2] Dexterity   " << "\n"
+             << "[3] Constitution" << "\n"
+             << "[4] Intelligence" << "\n"
+             << "[5] Wisdom      " << "\n"
+             << "[6] Charisma    " << "\n";
+        cin >> plrCInput;
+        switch (plrCInput) {
+            case 1:
+            // strength
+                attributePoints();
+                plrS = attribInput;
+                break;
+            case 2:
+            // dexterity
+                attributePoints();
+                plrD = attribInput;
+                break;
+            case 3:
+            // constitution
+                attributePoints();
+                plrC = attribInput;
+                break;
+            case 4:
+            // intelligence
+                attributePoints();
+                plrI = attribInput;
+                break;
+            case 5:
+            // wisdom
+                attributePoints();
+                plrW = attribInput;
+                break;
+            case 6:
+            // charisma
+                attributePoints();
+                plrCh = attribInput;
+                break;
+        }
     }
+    cout << "Strength:     " << plrS << endl
+         << "Dexterity:    " << plrD << endl
+         << "Constitution: " << plrC << endl
+         << "Intelligence: " << plrI << endl
+         << "Wisdom:       " << plrW << endl
+         << "Charisma:     " << plrCh << endl;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -20,28 +20,28 @@ int plrHp,
 int main() {
     characterCreation();
     Character player(plrHp, plrSp, plrS, plrD, plrC, plrI, plrW, plrCh, plrAP, plrName);
-/*
-*   For testing Purposes
-*   cout << "\n"
-*        << "Name:         " << player.name
-*        << "\n"
-*        << "Hitpoints:    " << player.hp
-*        << "\n"
-*        << "Spellpoints:  " << player.sp
-*        << "\n"
-*        << "Strength:     " << player.strength
-*        << "\n"
-*        << "Dexterity:    " << player.dexterity
-*        << "\n"
-*        << "Constitution: " << player.constitution
-*        << "\n"
-*        << "Intelligence: " << player.intelligence
-*        << "\n"
-*        << "Wisdom:       " << player.wisdom
-*        << "\n"
-*        << "Charisma:     " << player.charisma
-*        << "\n";
-*           return 0;
-*   End of Test
+/**
+  *   For testing Purposes
+  *   cout << "\n"
+  *        << "Name:         " << player.name
+  *        << "\n"
+  *        << "Hitpoints:    " << player.hp
+  *        << "\n"
+  *        << "Spellpoints:  " << player.sp
+  *        << "\n"
+  *        << "Strength:     " << player.strength
+  *        << "\n"
+  *        << "Dexterity:    " << player.dexterity
+  *        << "\n"
+  *        << "Constitution: " << player.constitution
+  *        << "\n"
+  *        << "Intelligence: " << player.intelligence
+  *        << "\n"
+  *        << "Wisdom:       " << player.wisdom
+  *        << "\n"
+  *        << "Charisma:     " << player.charisma
+  *        << "\n";
+  *           return 0;
+  *   End of Test
 */
 }
